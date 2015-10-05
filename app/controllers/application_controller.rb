b0ef4eda0173643b72729ded00e4387edb733233
Gitman <t.tuck303+github@gmail.com>
2015-10-05 14:58:48 -0600

    Exercises: goodbye world with new home router.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "¡hola, mundo"
+  	render text: "¡hola, mundo!"
+  end
+
+  def goodbye
+  	render text: "goodbye, world!"
   end
 
 end
