add878ced8f45385852ea1a79dd84a97764ba1da
Gitman <t.tuck303+github@gmail.com>
2015-10-05 14:56:19 -0600

    Exercise: hola mundo.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render text: "Hello, World!"
+  	render text: "¡hola, mundo"
   end
-  
+
 end
